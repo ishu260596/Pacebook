e6722df23282150804302de114978453eb613223
masai <varshakumari8999@gmail.com>
2021-06-10 13:15:24 +0530

    Login Signup Page

---
+++ app/src/main/java/com/ishwar_arcore/pacebook/views/ForgetPasswordActivity.java
@@ -0,0 +1,71 @@
+package com.ishwar_arcore.pacebook.views;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ishwar_arcore.pacebook.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private Button resetPasswordButton;
+    private ProgressBar progressBar;
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+
+        emailEditText=(EditText)findViewById(R.id.etForgetPassword);
+        resetPasswordButton=(Button)findViewById(R.id.btnReset);
+        progressBar=(ProgressBar)findViewById(R.id.progressbar3);
+        auth=FirebaseAuth.getInstance();
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
+            }
+        });
+
+    }
+
+    private void resetPassword() {
+        String email=emailEditText.getText().toString().trim();
+        if (email.isEmpty()){
+            emailEditText.setError("Email is required");
+            emailEditText.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Please provide valid email");
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(ForgetPasswordActivity.this,"check your email to reset your password!",Toast.LENGTH_LONG).show();
+
+                }else {
+                    Toast.makeText(ForgetPasswordActivity.this,"Try again! Something wrong happend!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/ishwar_arcore/pacebook/views/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.ishwar_arcore.pacebook.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ishwar_arcore.pacebook.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
